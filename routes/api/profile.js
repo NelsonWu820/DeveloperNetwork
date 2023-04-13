b1d8093057c2646f4c561860aa8ac34a34ad47bc
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-13 11:11:37 -0400

    added a route to get a specific user's profile which uses the checkObjectId to see if id is of the valid type

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const checkObjectId = require('../../middleware/checkObjectId');
 const { check, validationResult } = require('express-validator');
 const normalize = import('normalize-url');
 
@@ -30,9 +31,7 @@ router.get('/me', auth, async (req, res) => {
 // @route    POST api/profile
 // @desc     Create or update user profile
 // @access   Private
-router.post(
-    '/',
-    auth,
+router.post('/', auth,
     check('status', 'Status is required').notEmpty(),
     check('skills', 'Skills is required').notEmpty(),
     async (req, res) => {
@@ -106,5 +105,24 @@ router.get('/', async (req, res) => {
     }
   });
   
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', checkObjectId('user_id'),
+    async ({ params: { user_id } }, res) => {
+      try {
+        const profile = await Profile.findOne({
+          user: user_id
+        }).populate('user', ['name', 'avatar']);
+  
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+  
+        return res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ msg: 'Server error' });
+      }
+    }
+  );
 
 module.exports = router;
