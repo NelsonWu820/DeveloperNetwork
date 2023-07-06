f1aecd287367b3446f29e935c73466aba6e70cb2
Nelson Wu <Nelsonwu820@gmail.com>
2023-07-06 08:33:38 -0400

    debugging redux alert

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,8 +2,8 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = [];
 
-export default function(state = initialState, action) {
-    const { type, payload} = action
+function alertReducer(state = initialState, action) {
+    const { type, payload} = action;
 
     switch (type){
         case SET_ALERT:
@@ -14,3 +14,5 @@ export default function(state = initialState, action) {
             return state;
     }
 }
+
+export default alertReducer;
