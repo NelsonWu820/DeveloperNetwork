2d5885786c965283008aed7a2fe14bb5ca5bf8d0
Nelson Wu <Nelsonwu820@gmail.com>
2023-07-07 11:17:29 -0400

    when successfuly passed it will then register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert} from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types'
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -18,12 +19,12 @@ const Register = ({ setAlert }) => {
         setFormData({...formData, [e.target.name]: e.target.value })
     }
 
-    const onSubmit = async (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
         if (password !== password2) {
             setAlert("Passwords don't match", "danger");
         } else {
-            console.log( formData )
+            register({ name, email, password});
         }
     };
 
@@ -33,10 +34,10 @@ const Register = ({ setAlert }) => {
         <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
         <form className="form" onSubmit={onSubmit}>
             <div className="form-group">
-            <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+            <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}  />
             </div>
             <div className="form-group">
-            <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
+            <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
             <small className="form-text"
                 >This site uses Gravatar so if you want a profile image, use a
                 Gravatar email</small>
@@ -46,8 +47,7 @@ const Register = ({ setAlert }) => {
                 type="password"
                 placeholder="Password"
                 name="password"
-                minLength="6"
-                value={password} onChange={e => onChange(e)} required
+                value={password} onChange={e => onChange(e)} 
             />
             </div>
             <div className="form-group">
@@ -55,8 +55,7 @@ const Register = ({ setAlert }) => {
                 type="password"
                 placeholder="Confirm Password"
                 name="password2"
-                minLength="6"
-                value={password2} onChange={e => onChange(e)} required
+                value={password2} onChange={e => onChange(e)} 
             />
             </div>
             <input type="submit" className="btn btn-primary" value="Register" />
@@ -69,7 +68,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-    setAlert : PropTypes.func.isRequired
+    setAlert : PropTypes.func.isRequired,
+    register : PropTypes.func.isRequired
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
