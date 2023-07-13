e08745ba89c18172574d5c0044830893500aa790
Nelson Wu <Nelsonwu820@gmail.com>
2023-07-13 07:56:30 -0400

    added a case for a loaded user success and and auth failure

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import { USER_LOADED, AUTH_ERROR, REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -10,6 +10,13 @@ const initialState = {
 function authReducer(state = initialState, action) {
     const { type, payload } = action;
     switch(type){
+        case USER_LOADED:
+            return {
+              ...state,
+              isAuthenticated: true,
+              loading: false,
+              user: payload
+            };
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -19,6 +26,7 @@ function authReducer(state = initialState, action) {
                 loading: false
             };  
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             return {
                 ...state,
                 token: null,
