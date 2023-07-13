92489c7e21cd3914a29a628c834a067d9c3a0d37
Nelson Wu <Nelsonwu820@gmail.com>
2023-07-13 07:57:41 -0400

    added an initial check to see if they have a correct token using useEffect to call loaduser function in actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,8 +9,18 @@ import Alert from './components/layout/Alert';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
-function App() {
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser), []
+  });
+  
   return (
     <Provider store={store}>
       <Router>
