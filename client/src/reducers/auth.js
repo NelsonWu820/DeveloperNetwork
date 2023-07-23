b9a158ffa69e7318b384dbdc72691b7937d43cc5
Nelson Wu <nelsonwu820@gmail.com>
2023-07-23 09:44:24 -0400

    added a case for login success and fail

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOADED, AUTH_ERROR, REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import { USER_LOADED, AUTH_ERROR, REGISTER_SUCCESS, REGISTER_FAIL, LOGIN_SUCCESS, LOGIN_FAIL } from "../actions/types";
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -18,6 +18,7 @@ function authReducer(state = initialState, action) {
               user: payload
             };
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
@@ -27,6 +28,7 @@ function authReducer(state = initialState, action) {
             };  
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             return {
                 ...state,
                 token: null,
