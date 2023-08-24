dd744e66ab49004e1138eee93e9eb36e3131e784
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-24 12:18:51 -0400

    created a Dashboard route

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import { Link } from 'react-router-dom';
+
+
+const Dashboard = ({ getCurrentProfile, auth: {user}, profile: {profile, loading} }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
+
+    return (
+    <section className="container">
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user" /> Welcome {user && user.name}
+        </p>
+        {profile !== null ? (
+          <>
+            <div className="my-2">
+                Has Account
+            </div>
+          </>
+        ) : (
+          <>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </>
+        )}
+    </section>
+    );
+};
+
+
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+})
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
