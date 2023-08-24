d364cc56b615b5e7b73731363a2ad98d65ccb234
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-24 12:26:06 -0400

    added profile clear

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,5 +7,6 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+export const PROFILE_CLEAR = 'PROFILE_CLEAR';
 export const PROFILE_GET = 'PROFILE_GET';
 export const PROFILE_ERROR = 'PROFILE_ERROR'; 
