9895ca5e23b6666b5d66f259f04e9cb9db092dda
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-24 12:16:07 -0400

    added a dashboard protected route

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,26 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
 
-if(localStorage.token){
-  setAuthToken(localStorage.token);
-}
 
 const App = () => {
   useEffect(() => {
-    store.dispatch(loadUser), []
-  });
+    if(localStorage.token){
+      setAuthToken(localStorage.token);
+    }
+
+    store.dispatch( loadUser());
+  }, []);
   
   return (
-    <Provider store={store}>
+    <Provider store={store} className='container'>
       <Router>
       <Navbar/>
       <Alert/>
@@ -30,6 +33,7 @@ const App = () => {
             <Route path='/' element={ <Landing /> }/>
             <Route path="login" element={ <Login /> }/>
             <Route path="register" element={ <Register /> }/>
+            <Route path="dashboard" element={<PrivateRoute  component={ Dashboard }/>} />
         </Routes>
       </Router>
     </Provider>
