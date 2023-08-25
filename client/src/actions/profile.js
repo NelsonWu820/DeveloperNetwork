03eaad01bbb634bb3c5f5a8b43117fcb668543cd
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-25 11:16:14 -0400

    changed axios to util/api call

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,9 +1,10 @@
 import { PROFILE_GET, PROFILE_ERROR } from "./types";
-import axios from "axios";
+import api from "../utils/api";
+import { setAlert } from "./alert";
 
 export const getCurrentProfile = () => async dispatch => {
     try {
-        const res = await axios('/api/profile/me');
+        const res = await api.get('/api/profile/me');
 
         dispatch({
             type: PROFILE_GET,
@@ -16,3 +17,31 @@ export const getCurrentProfile = () => async dispatch => {
         })
     }
 }
+
+// Create or update profile
+export const createProfile = (formData, edit = false) => async (dispatch) => {
+    try {
+      const res = await api.post('/profile', formData);
+
+      dispatch({
+        type: PROFILE_GET,
+        payload: res.data
+      });
+
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      );
+
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
