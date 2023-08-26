986ec6bbc30f8bdc64770b3024e63d79cf5da1f0
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-26 12:41:19 -0400

    added the delete action for experiences, education, and account

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,4 @@
-import { PROFILE_GET, PROFILE_ERROR, PROFILE_UPDATE } from "./types";
+import { PROFILE_GET, PROFILE_ERROR, PROFILE_UPDATE, ACCOUNT_DELETE, PROFILE_CLEAR } from "./types";
 import api from "../utils/api";
 import { setAlert } from "./alert";
 
@@ -97,3 +97,81 @@ export const addEducation = (formData) => async (dispatch) => {
     });
   }
 };
+
+//delete Experience 
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await api.delete(`/profile/experience/${id}`);
+
+    dispatch({
+      type: PROFILE_UPDATE,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Experience Deleted', 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+//delete Experience 
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await api.delete(`/profile/education/${id}`);
+
+    dispatch({
+      type: PROFILE_UPDATE,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Education Deleted', 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+//Delete account & profile
+export const deleteAccount = id => async dispatch => {
+  if(window.confirm("Are you sure? This action can NOT be undone")){
+    try {
+      await api.delete('/profile');
+  
+      dispatch({PROFILE_CLEAR})
+      dispatch({
+        type: ACCOUNT_DELETE
+      })
+
+  
+      dispatch(setAlert('Account Deleted'));
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+  
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+}
