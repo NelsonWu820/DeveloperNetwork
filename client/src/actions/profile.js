f6a6e7828d0bfdbb9995d0b4099b9bebb2bd224f
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-26 09:46:08 -0400

    added add Education and Experience actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,10 +1,10 @@
-import { PROFILE_GET, PROFILE_ERROR } from "./types";
+import { PROFILE_GET, PROFILE_ERROR, PROFILE_UPDATE } from "./types";
 import api from "../utils/api";
 import { setAlert } from "./alert";
 
 export const getCurrentProfile = () => async dispatch => {
     try {
-        const res = await api.get('/api/profile/me');
+        const res = await api.get('/profile/me');
 
         dispatch({
             type: PROFILE_GET,
@@ -45,3 +45,55 @@ export const createProfile = (formData, edit = false) => async (dispatch) => {
       });
     }
   };
+
+// Add Experience
+export const addExperience = (formData) => async (dispatch) => {
+  try {
+    const res = await api.put('/profile/experience', formData);
+
+    dispatch({
+      type: PROFILE_UPDATE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Added', 'success'));
+    return res.data;
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Add Education
+export const addEducation = (formData) => async (dispatch) => {
+  try {
+    const res = await api.put('/profile/education', formData);
+
+    dispatch({
+      type: PROFILE_UPDATE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Added', 'success'));
+    return res.data;
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
