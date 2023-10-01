4536d76ebc9223e69be45473f8ff6713de8529f8
Nelson Wu <Nelsonwu820@gmail.com>
2023-10-01 08:54:49 -0400

    added a multi window listener so logout in one logs out all

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts'; 
 import Post from './components/post/Post';
+import { LOGOUT } from './actions/types';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -25,11 +26,19 @@ import { loadUser } from './actions/auth';
 
 const App = () => {
   useEffect(() => {
-    if(localStorage.token){
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
       setAuthToken(localStorage.token);
     }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser());
 
-    store.dispatch( loadUser());
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
   }, []);
   
   return (
