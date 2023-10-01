60b2a5bf4457de64f9b330e9d6ecd71b41beeed7
Nelson Wu <Nelsonwu820@gmail.com>
2023-10-01 10:51:18 -0400

    added production route to use index.html as default

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/db")
+const path = require('path');
 
 const app = express();
 
+//connects to mongo
 connectDB();
 
 app.use(express.json({ extended: false}));
@@ -14,6 +16,17 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    //gets the index.html from client/build
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //defualt port is 5000
 const PORT = process.env.PORT || 5000;
