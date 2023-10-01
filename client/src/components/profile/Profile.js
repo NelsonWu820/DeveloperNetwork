f71981e7edb83fa1d87840eca79a9d818f4dacd0
Nelson Wu <Nelsonwu820@gmail.com>
2023-10-01 08:55:12 -0400

    adde btn styling to be below navbar

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,17 +18,20 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 
     return (
         <Fragment>
-          {profile === null || loading ? <h1>Waiting</h1> : (<Fragment>Profile
-          <Link to='/profiles' className='btn btn-light'>
-              Back to Profiles
-          </Link>
-          {auth.isAuthenticated &&
-              auth.loading === false &&
-              auth.user._id === profile.user._id && (
-                  <Link to='/edit-profile' className='btn btn-dark'>
-                      Edit Profile
-                  </Link>
-              )}
+          {profile === null || loading ? <h1>Waiting</h1> : 
+          (<Fragment>
+          <div className='profile-btn-container'>
+            <Link to='/profiles' className='btn btn-light'>
+                Back to Profiles
+            </Link>
+            {auth.isAuthenticated &&
+                auth.loading === false &&
+                auth.user._id === profile.user._id && (
+                    <Link to='/edit-profile' className='btn btn-dark btn'>
+                        Edit Profile
+                    </Link>
+                )}
+          </div>
               <div className="profile-grid my-1">
                 <ProfileTop profile={profile}/>
                 <ProfileAbout profile={profile}/>
